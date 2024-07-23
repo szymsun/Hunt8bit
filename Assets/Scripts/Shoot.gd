7aad87d89c070fb132bf6c98c9fe4b8fe7c96d44
szymsun <szymson713@gmail.com>
2024-07-23 18:51:42 +0200

    Started working on ammo. TODO: monke

--- Assets/Scripts/Shoot.gd
+++ Assets/Scripts/Shoot.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-const BULLET = preload("res://Assets/Playables/bullet.tscn")
-
-
-var ammo = int()
+var ammo = 300
 
 
 @onready var shoot_audio = $ShootAudio
@@ -14,7 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	
 	if Input.is_action_just_pressed("shoot") and ammo > 0:
 		shoot_audio.play()
-
+		var bullet = load("res://Assets/Playables/bullet.tscn")
+		ammo -= 1
